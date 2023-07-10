baa181a8f2e59f21df8ec46455b8a2bba7d42f3f
Artjoms <artjoms.voroncovs@gmail.com>
2023-07-10 14:45:55 +0300

    feat: Updated code generation module * Added code generation for negation expression * Added code generation for bitwise complement expression * Added code generation for logical negation expression

--- generate/generate.c
+++ generate/generate.c
@@ -49,14 +49,45 @@ void generate_expression(struct TreeNode * node){
 
 	for(int i = node->children_amount - 1; i >= 0; i--){
 		child_node = node->children[i];
-	
+
 		if(strcmp(child_node->type, "INT") == 0) {
 			asm_buffer_ptr += sprintf(asm_buffer_ptr, "movl $%s, %%eax\n", child_node->value);	
 		}
+
+		if(strcmp(child_node->type, "EXPRESSION") == 0) {
+			generate_expression(child_node);
+		}
+
+		if(strcmp(child_node->type, "UNARY_OP") == 0) {
+			generate_unary_op(child_node->children[0]);
+		}
+
 		else {
 			if(SILENT_ARG != true)
 				printf("No handler\n");
 		}
+	}
+}
+
+void generate_unary_op(struct TreeNode * node){
+	if(strcmp(node->type, "NEGATION_OP") == 0) {
+		asm_buffer_ptr += sprintf(asm_buffer_ptr, "\n");	
+		asm_buffer_ptr += sprintf(asm_buffer_ptr, "# NEGATION OP\n");	
+		asm_buffer_ptr += sprintf(asm_buffer_ptr, "neg %%eax\n");	
+	}
+
+	if(strcmp(node->type, "BITWISE_COMPLEMENT_OP") == 0) {
+		asm_buffer_ptr += sprintf(asm_buffer_ptr, "\n");	
+		asm_buffer_ptr += sprintf(asm_buffer_ptr, "# BITWISE COMPLEMENT OP\n");	
+		asm_buffer_ptr += sprintf(asm_buffer_ptr, "not %%eax\n");	
+	}
+
+	if(strcmp(node->type, "LOGICAL_NEGATION_OP") == 0) {
+		asm_buffer_ptr += sprintf(asm_buffer_ptr, "\n");	
+		asm_buffer_ptr += sprintf(asm_buffer_ptr, "# LOGICAL NEGATION OP\n");	
+		asm_buffer_ptr += sprintf(asm_buffer_ptr, "cmpl $0, %%eax\n");	
+		asm_buffer_ptr += sprintf(asm_buffer_ptr, "movl $0, %%eax\n");	
+		asm_buffer_ptr += sprintf(asm_buffer_ptr, "sete %%al\n");	
 	}
 }
 
--- generate/generate.h
+++ generate/generate.h
@@ -3,4 +3,5 @@ void generate_program(struct TreeNode * node);
 void generate_function(struct TreeNode * node);
 void generate_statement(struct TreeNode * node);
 void generate_expression(struct TreeNode * node);
+void generate_unary_op(struct TreeNode * node);
 
