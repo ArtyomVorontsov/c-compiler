ab4ff6eb2a956fad69f5419b56f7aabcf23d3091
Artjoms <artjoms.voroncovs@gmail.com>
2023-07-13 08:34:05 +0300

    feat: extended code generation

--- generate/generate.c
+++ generate/generate.c
@@ -7,6 +7,8 @@ void generate(struct TreeNode * root_node){
 }
 
 void generate_program(struct TreeNode * node){
+	printf("generate_program\n");
+	
 	if(strcmp(node->type, "FUNCTION") == 0){
 			generate_function(node);
 	}
@@ -17,6 +19,7 @@ void generate_program(struct TreeNode * node){
 }
 
 void generate_function(struct TreeNode * node){
+	printf("generate_function\n");
 	char *identifier_value = node->children[1]->value;
 	struct TreeNode * statement_node = node->children[5];
 	asm_buffer_ptr += sprintf(asm_buffer_ptr, ".globl %s\n", identifier_value);
@@ -26,13 +29,14 @@ void generate_function(struct TreeNode * node){
 }
 
 void generate_statement(struct TreeNode * node){
+	printf("generate_statement\n");
 	struct TreeNode * child_node;
 
 	for(int i = node->children_amount - 1; i >= 0; i--){
 		child_node = node->children[i];
 	
 		if(strcmp(child_node->type, "EXPRESSION") == 0) {
-			generate_expression(child_node);
+			generate_expression(&child_node);
 		} 
 		else if(strcmp(child_node->type, "RETURN_KEYWORD") == 0) {
 			asm_buffer_ptr += sprintf(asm_buffer_ptr, "ret\n");	
@@ -44,24 +48,35 @@ void generate_statement(struct TreeNode * node){
 	}
 }
 
-void generate_expression(struct TreeNode * node){
+void generate_expression(struct TreeNode ** node){
+	printf("generate_expression\n");
+	//printf("generate_expression %s\n", (**node).type);
 	struct TreeNode * child_node;
 
-	for(int i = node->children_amount - 1; i >= 0; i--){
-		child_node = node->children[i];
+	for(int i = 0; i < (*node)->children_amount - 1; i++){
+		child_node = (*node)->children[i];
 
-		if(strcmp(child_node->type, "INT") == 0) {
-			asm_buffer_ptr += sprintf(asm_buffer_ptr, "movl $%s, %%eax\n", child_node->value);	
-		}
 
 		if(strcmp(child_node->type, "EXPRESSION") == 0) {
-			generate_expression(child_node);
+			generate_expression(&child_node);
 		}
 
-		if(strcmp(child_node->type, "UNARY_OP") == 0) {
-			generate_unary_op(child_node->children[0]);
-		}
+		printf("%d\n", i);
+		printf("generate_expression %s\n", (*node)->children[i + 1]->type);
+		if(strcmp(child_node->type, "TERM") == 0) {
 
+			if(strcmp((*node)->children[i + 1]->type, "ADDITION_OP") == 0) {
+				printf("ADDITION %s\n", child_node->type);
+				generate_fact((*node)->children[i]);
+				asm_buffer_ptr += sprintf(asm_buffer_ptr, "push %%eax\n");
+				generate_fact((*node)->children[i + 2]);
+				asm_buffer_ptr += sprintf(asm_buffer_ptr, "pop %%ecx\n");
+				asm_buffer_ptr += sprintf(asm_buffer_ptr, "addl %%ecx %%eax\n");
+				node = node + 3;
+				printf("DONE\n");
+			}
+//			generate_term(&child_node);
+		}
 		else {
 			if(SILENT_ARG != true)
 				printf("No handler\n");
@@ -70,6 +85,7 @@ void generate_expression(struct TreeNode * node){
 }
 
 void generate_unary_op(struct TreeNode * node){
+	printf("generate_unary_op\n");
 	if(strcmp(node->type, "NEGATION_OP") == 0) {
 		asm_buffer_ptr += sprintf(asm_buffer_ptr, "\n");	
 		asm_buffer_ptr += sprintf(asm_buffer_ptr, "# NEGATION OP\n");	
@@ -90,4 +106,33 @@ void generate_unary_op(struct TreeNode * node){
 		asm_buffer_ptr += sprintf(asm_buffer_ptr, "sete %%al\n");	
 	}
 }
+
+void generate_term(struct TreeNode ** node){
+	printf("generate_term\n");
+	/*if(strcmp((*node + 1)->type, "ADDITION_OP") == 0) {
+		generate_fact(node);
+		asm_buffer_ptr += sprintf(asm_buffer_ptr, "push %%eax\n");
+		generate_fact(node + 2);
+		asm_buffer_ptr += sprintf(asm_buffer_ptr, "pop %%ecx\n");
+		asm_buffer_ptr += sprintf(asm_buffer_ptr, "addl %%ecx %%eax\n");
+		node = node + 3;
+	}*/
+	/*if(strcmp(node->type, "ADDITION_OP") == 0) {
+			asm_buffer_ptr += sprintf(asm_buffer_ptr, "movl $%s, %%eax\n", child_node->value);	
+	}*/
+}
+
+void generate_fact(struct TreeNode * node){
+	printf("generate_fact\n");
+	struct TreeNode * child_node = node;
+
+	if(strcmp(child_node->type, "INT") == 0) {
+		asm_buffer_ptr += sprintf(asm_buffer_ptr, "movl $%s, %%eax\n", child_node->value);	
+	}
+
+	if(strcmp(child_node->type, "UNARY_OP") == 0) {
+		generate_unary_op(child_node->children[0]);
+	}
+}
+
 
--- generate/generate.h
+++ generate/generate.h
@@ -2,6 +2,8 @@
 void generate_program(struct TreeNode * node);
 void generate_function(struct TreeNode * node);
 void generate_statement(struct TreeNode * node);
-void generate_expression(struct TreeNode * node);
+void generate_expression(struct TreeNode ** node);
+void generate_term(struct TreeNode ** node);
+void generate_fact(struct TreeNode * node);
 void generate_unary_op(struct TreeNode * node);
 
