a493900f038888b1d7935b0b977f236c32b98561
Artjoms <artv20022@gmail.com>
2023-08-25 14:06:55 +0300

    feat: Improve analyzer

--- analyzer/analyzer.c
+++ analyzer/analyzer.c
@@ -12,17 +12,21 @@ struct FunctionRecord * records[100];
 int function_records_amount = 0;
 
 void analyze(struct TreeNode * node){
-	traverse_tree(node);
+	print_if_explicit("Analysis started.\n\n");
+
+	tree_traverse_1(node);
+	tree_traverse_2(node);
 }
 
-void traverse_tree(struct TreeNode * node){
+void tree_traverse_1(struct TreeNode * node){
 	int i;
 	struct FunctionRecord * fn_record;
 	char * function_name;
 	int function_args_amount;
-	 
 
 	if(strcmp("FUNCTION_DECLARATION", node->type) == 0){
+		print_if_explicit("FUNCTION_DECLARAION\n");
+
 		function_name = node->children[1]->value;
 		function_args_amount = node->children[2]->children_amount;
 		fn_record = find_function_record(function_name);
@@ -32,9 +36,18 @@ void traverse_tree(struct TreeNode * node){
 			exit(1);
 		}
 
-		register_function_declaration(function_name, function_args_amount);
+
+		if(fn_record != -1 && fn_record->declared == false){
+			fn_record->declared = true;
+			fn_record->declared_args_amount = function_args_amount;
+		} 
+		else {
+			register_function_declaration(function_name, function_args_amount);
+		}
 	}
 	else if(strcmp("FUNCTION_DEFINITION", node->type) == 0){
+		print_if_explicit("FUNCTION_DEFINITION\n");
+
 		function_name = node->children[1]->value;
 		function_args_amount = node->children[2]->children_amount;
 		fn_record = find_function_record(function_name);
@@ -49,12 +62,78 @@ void traverse_tree(struct TreeNode * node){
 			exit(1);
 		}
 
-		register_function_definition(function_name, function_args_amount);
+		if(fn_record != -1 && fn_record->defined == false){
+			fn_record->defined = true;
+			fn_record->defined_args_amount = function_args_amount;
+		} else {
+			register_function_definition(function_name, function_args_amount);
+		}
 
 	}
 
 	for(i = 0; i < node->children_amount; i++){
-		traverse_tree(node->children[i]);
+		tree_traverse_1(node->children[i]);
+	}
+}
+
+void tree_traverse_2(struct TreeNode * node){
+	int i;
+	struct FunctionRecord * fn_record;
+	char * function_name;
+	int function_args_amount;
+
+	if(strcmp("FUNCTION_DECLARATION", node->type) == 0){
+		print_if_explicit("FUNCTION_DECLARAION\n");
+
+		function_name = node->children[1]->value;
+		function_args_amount = node->children[2]->children_amount;
+		fn_record = find_function_record(function_name);
+
+
+	}
+	else if(strcmp("FUNCTION_DEFINITION", node->type) == 0){
+		print_if_explicit("FUNCTION_DEFINITION\n");
+
+		function_name = node->children[1]->value;
+		function_args_amount = node->children[2]->children_amount;
+		fn_record = find_function_record(function_name);
+
+	}
+	else if(strcmp("FUNCTION_CALL", node->type) == 0){
+		print_if_explicit("FUNCTION_CALL\n");
+
+		function_name = node->children[0]->value;
+		function_args_amount = node->children_amount == 2 ? node->children[1]->children_amount : 0;
+		fn_record = find_function_record(function_name);
+
+		if(fn_record == -1 || (fn_record->declared == false && fn_record->defined == false)){
+			printf("ERROR: function %s is not declared or defined.\n", function_name);
+			exit(1);
+		}
+
+		if(fn_record->defined && fn_record->defined_args_amount > function_args_amount) {
+			printf("ERROR: in function call of %s arguments amount is smaller than in function definition.\n", function_name);
+			exit(1);
+		}
+
+		if(fn_record->defined && fn_record->defined_args_amount < function_args_amount) {
+			printf("ERROR: in function call of %s arguments amount is larger than in function definition.\n", function_name);
+			exit(1);
+		}
+
+		if(fn_record->declared && fn_record->declared_args_amount > function_args_amount) {
+			printf("ERROR: in function call of %s arguments amount is smaller than in function declaration.\n", function_name);
+			exit(1);
+		}
+
+		if(fn_record->declared && fn_record->declared_args_amount < function_args_amount) {
+			printf("ERROR: in function call of %s arguments amount is larger than in function declaration.\n", function_name);
+			exit(1);
+		}
+	}
+
+	for(i = 0; i < node->children_amount; i++){
+		tree_traverse_2(node->children[i]);
 	}
 }
 
--- analyzer/analyzer.h
+++ analyzer/analyzer.h
@@ -1,6 +1,7 @@
 #include "../compiler.h"
 
-void traverse_tree(struct TreeNode * node);
+void tree_traverse_1(struct TreeNode * node);
+void tree_traverse_2(struct TreeNode * node);
 struct FunctionRecord * find_function_record(char * name);
 struct FunctionRecord * register_function_declaration(char * name, int args_amount);
 struct FunctionRecord * register_function_definition(char * name, int args_amount);
